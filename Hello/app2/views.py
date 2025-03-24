4403695aaaf484c5fa8cc44d20f354484e718003
AnkitKolhe149 <nkolhe149@gmail.com>
2025-03-24 16:27:57 +0530

    first commit

---
+++ Hello/app2/views.py
@@ -0,0 +1,22 @@
+#from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render,HttpResponse
+# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+    #return HttpResponse("This is home page from index")
+
+
+def about(request):
+    return render(request, 'about.html') 
+    # return HttpResponse("This is about page")
+
+def contact(request):
+    return render(request, 'contact.html')
+
+# def services(request):
+#     return render(request, 'services.html')
+
+def home(request):
+    return render(request, 'home.html')
